d7084ce97b7d871190101376e9149fd45426bb3c
Almighty Stranger <sufyankhan9678@gmail.com>
2020-12-31 17:56:07 +0530

    Backend Ready

---
+++ data/customers.js
@@ -0,0 +1,5 @@
+const customers=[
+   {name:"customer1",phone:"9897323445",address:"Address 1",email:"Example@gmail.com"}
+
+]
+export default customers;
---
+++ data/vendors.js
@@ -0,0 +1,5 @@
+const vendors=[
+    {name:"vendor1",phone:"9897323445",address:"Address 1",email:"Example@gmail.com"}
+ 
+ ]
+ export default vendors;
